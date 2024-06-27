0451e5522e3a0dde457b73fa5b9c6c27ea2e9270
CraftedDevCodeMaster <praveenkumarr200025@gmail.com>
2024-06-27 19:30:29 +0530

    dashbord changes

--- eesweb/src/main/java/com/ess/web/EeswebApplication.java
+++ eesweb/src/main/java/com/ess/web/EeswebApplication.java
@@ -3,8 +3,6 @@ package com.ess.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ess.web.entity.EssUser;
-
 @SpringBootApplication
 public class EeswebApplication {
 
--- eesweb/src/main/java/com/ess/web/controller/LoginController.java
+++ eesweb/src/main/java/com/ess/web/controller/LoginController.java
@@ -1,8 +1,25 @@
 package com.ess.web.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class LoginController {
-	
+
+	@GetMapping("/login")
+	public String loginForm() {
+		return "login";
+	}
+
+	@PostMapping("/loginform")
+	public String loginForm1() {
+		return "essDashboard";
+	}
+
+	@GetMapping("/essDashboard")
+	public String essDashboard() {
+		return "essDashboard";
+	}
+
 }
