a593ce818f7fa8fbf3793a70dc13f7422c48c464
CraftedDevCodeMaster <praveenkumarr200025@gmail.com>
2024-06-27 20:28:53 +0530

    spring boot version 3.3.1 updated

--- eesweb/src/main/java/com/ess/web/EssSpringSecurityConfig.java
+++ eesweb/src/main/java/com/ess/web/EssSpringSecurityConfig.java
@@ -1,35 +1,34 @@
 package com.ess.web;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.util.Assert;
 
 @Configuration
 @EnableWebSecurity
-@ConditionalOnExpression("'${Ess_security}'.equals('Ess')")
 public class EssSpringSecurityConfig {
 
 	@Autowired
 	private AuthenticationProvider essAuthenticationProvider;
 
-	@Autowired
 	public EssSpringSecurityConfig(AuthenticationProvider essAuthenticationProvider) {
-		Assert.notNull(essAuthenticationProvider, "AUTH not be null");
 		this.essAuthenticationProvider = essAuthenticationProvider;
 	}
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authenticationProvider(essAuthenticationProvider)
-				.authorizeRequests(authorize -> authorize.requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
-						.requestMatchers("/login").permitAll().anyRequest().authenticated())
-				.formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/essDashboard"));
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+        http.cors(withDefaults()).csrf(withDefaults()).authenticationProvider(essAuthenticationProvider)
+                .authorizeRequests(authorize -> authorize.requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/login").permitAll().anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/essDashboard"));
 		return http.build();
 	}
 }
